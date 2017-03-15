4e665a14285ab21b992afc0348bc0067503fbab3
Alex.Dai <alex.dai@fortune-inc.com>
2017-03-15 13:52:20 +0800

    first commit

---
+++ components/Bio.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { config } from 'config'
+import { rhythm } from 'utils/typography'
+import { prefixLink } from 'gatsby-helpers'
+import profilePic from './profile-pic.png'
+
+class Bio extends React.Component {
+  render () {
+    return (
+      <p
+        style={{
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={prefixLink(profilePic)}
+          alt={`author ${config.authorName}`}
+          style={{
+            float: 'left',
+            marginRight: rhythm(1/4),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+            "-webkit-border-radius": "50%",
+          }}
+        />
+        Written by <strong>{config.authorName}</strong>
+      </p>
+    )
+  }
+}
+
+export default Bio
---
+++ pages/2017-03-06-first-post/index.md
@@ -0,0 +1,9 @@
+---
+title: First Post
+date: "2017-03-07 15:00:00"
+readNext: ""
+path: "/first-post/"
+author: "Alex.Dai"
+---
+
+This is my first post!
